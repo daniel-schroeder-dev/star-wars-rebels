64cfb56dd6813ee360142044d1273110b5a00caa
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-12 21:55:31 -0500

    Add some heroku configs, setup express.static middleware.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
 
 app.listen(PORT, () => {
   console.log('Rebels up at:', PORT);
