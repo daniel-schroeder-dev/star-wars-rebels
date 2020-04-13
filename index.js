85f1d2a5ae607dbf693349396ca97299ed916abf
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-12 22:21:25 -0500

    Allow user to reload page on rebel link.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rebelDetails = require('./fixtures/rebel-details');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -10,12 +11,12 @@ app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/rebel-details', (req, res, next) => {
-  res.json(require('./fixtures/rebel-details'));
+  res.json(rebelDetails);
 });
 
 app.get('*', (req, res, next) => {
-  console.log(req.url);
-  res.render('index');
+  const rebel = rebelDetails[req.url.split('/')[1]];
+  res.render('index', { rebel });
 });
 
 app.listen(PORT, () => {
